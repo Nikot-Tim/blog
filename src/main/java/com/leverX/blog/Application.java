7280252950e26ff2b86fb87fe5c69faf733a29eb
Nikita <n_timoshchenko13@mail.ru>
2019-08-15 01:48:10 +0300

    Mail Sender

--- src/main/java/com/leverX/blog/Application.java
+++ src/main/java/com/leverX/blog/Application.java
@@ -2,7 +2,7 @@ package com.leverX.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//All
+//A
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
